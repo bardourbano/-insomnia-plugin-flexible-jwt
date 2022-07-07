cab13b10d7cffce2d5409eb1b758de773c67ba2b
João Melo <joao.melo@imusica.com.br>
2022-07-07 16:00:22 -0300

    first version

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-// For help writing plugins, visit the documentation to get started:
-//   https://docs.insomnia.rest/insomnia/introduction-to-plugins
+const jwt = require('jsonwebtoken');
 
-// TODO: Add plugin code here...
+module.exports.templateTags = [{
+    name: 'flexibleJWT',
+    displayName: 'Flexible JWT',
+    description: 'Generates a JWT token',
+    args: [
+        {
+            displayName: 'JSON Payload',
+            type: 'string',
+            placeholder: /* JWT example */ '{\n"iss": "https://example.com",\n"sub": "1234567890",\n"aud": "https://example.com",\n"exp": "1409547200",\n"iat": "1409547200"}',
+            defaultValue: '{}'
+        },
+        {
+            displayName: 'Secret',
+            type: 'string',
+            placeholder: 'Secret',
+            defaultValue: ''
+        }
+    ],
+
+    async run(context, payload, secret) {
+        const header = {
+            algorithm: 'HS256',
+            type: 'JWT'
+        };
+
+        return jwt.sign(payload, secret, { algorithm: 'HS256', header });
+    }
+
+}]
